87946d11a1df33639defc7083fddd3d7e57c8d7a
Luis Sanchez <sanchez.luis589@ccm.student.edu>
2013-09-05 20:11:27 -0400

    First commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 1;
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+
+        public void count()
+        {
+            if (myInt == 1)
+            {
+
+
+    }
+}
